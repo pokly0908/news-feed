d1eedaaf4589135da6711d9f12406ac9e40fa2f2
pokly0908 <pokly0908@naver.com>
2024-02-07 18:55:41 +0900

    :sparkles: 기본추가

---
+++ src/main/java/com/sparta/newsfeed/entity/WIsh.java
@@ -0,0 +1,9 @@
+package com.sparta.newsfeed.entity;
+
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class WIsh {
+
+}
---
+++ src/main/java/com/sparta/newsfeed/service/WishService.java
@@ -0,0 +1,8 @@
+package com.sparta.newsfeed.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class WishService {
+
+}
