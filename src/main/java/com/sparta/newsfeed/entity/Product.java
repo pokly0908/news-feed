389876781f7a19085f47e8dbb154c772045e4846
pokly0908 <pokly0908@naver.com>
2024-02-07 14:57:52 +0900

    :sparkles: 기본적인 Entity 추가

--- src/main/java/com/sparta/newsfeed/entity/Product.java
+++ src/main/java/com/sparta/newsfeed/entity/Product.java
@@ -1,31 +1,38 @@
 package com.sparta.newsfeed.entity;
 
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
 
 @Entity
 @Getter
-@Table(name = "USER")
-public class User {
+@Table(name = "PRODUCT")
+public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long userId;
+    private Long productId;
 
-    @Column(nullable = false, unique = true)
-    private String nickname;
-
-    @Column(nullable = false, unique = true)
-    private String email;
+    @ManyToOne
+    @JoinColumn(name = "userId")
+    private User user;
 
     @Column(nullable = false)
-    private String password;
+    private String category;
 
     @Column(nullable = false)
-    private String userInfo;
+    private String title;
+
+    @Column(nullable = false)
+    private String productInfo;
+
+    @Column(nullable = false)
+    private int price;
 
 }
