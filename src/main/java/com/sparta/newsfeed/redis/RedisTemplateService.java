3d27ae8800bf71cc441bf76c4b6b6e3ce61b4a1d
newbe01 <pkwaa3@gmail.com>
2024-02-13 14:44:21 +0900

    expiration

--- src/main/java/com/sparta/newsfeed/redis/RedisTemplateService.java
+++ src/main/java/com/sparta/newsfeed/redis/RedisTemplateService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 @RequiredArgsConstructor
@@ -13,7 +14,11 @@ public class RedisTemplateService {
     private final RedisTemplate<String, String> redisTemplate;
 
     public void save(String email, String token, long expiration) {
-        redisTemplate.opsForValue().set(email, token, expiration, TimeUnit.MILLISECONDS);
+        long expirationTime = expiration - new Date().getTime();
+        if (expirationTime <= 0) {
+            throw new IllegalArgumentException("만료된 토큰입니다");
+        }
+        redisTemplate.opsForValue().set(email, token, expirationTime, TimeUnit.MILLISECONDS);
     }
 
     public String get(String email) {
