6b1620b2d0cc8b8b710a2b8943e0d8a348c4474d
pokly0908 <pokly0908@naver.com>
2024-03-14 20:14:22 +0900

    :recycle: code convention, readme 수정

--- src/test/java/com/sparta/newsfeed/controller/ControllerTest.java
+++ src/test/java/com/sparta/newsfeed/controller/ControllerTest.java
@@ -2,7 +2,6 @@ package com.sparta.newsfeed.controller;
 
 
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
